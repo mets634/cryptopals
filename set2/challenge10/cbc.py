47602fb6dd63a5438bd4a7e03703d14bc70e12f2
mets634 <mets634@gmail.com>
2017-10-09 19:24:31 +0300

    made encryption method

--- set2/challenge10/cbc.py
+++ set2/challenge10/cbc.py
@@ -5,9 +5,42 @@ import os
 os.sys.path.append(os.path.dirname(os.path.abspath('.')))
 
 from Crypto.Cipher import AES
+from challenge9 import pad
+from math import ceil
 
-def ecb_encode(msg, key):
+def sxor(str1, str2):
+    return ''.join(chr(ord(c1) ^ ord(c2)) 
+            for (c1, c2) in zip(str1, str2))
+
+def ecb_encrypt(msg, key):
     aes = AES.new(key, AES.MODE_ECB)
     return aes.encrypt(msg)
 
-def cbc_encode(iv, msg, 
+def ecb_decrypt(cipher, key):
+    aes = AES.new(key, MODE_ECB)
+    return aes.decrypt(cipher)
+
+def cbc_encrypt(iv, msg, key):
+    block = lambda k: msg[k * len(key) : (k + 1) * len(key)]
+
+    block_count = ceil(len(msg) * 1.0 / len(key))
+    
+    msg = pad(block_count * len(key))
+
+    prev_cipher = iv
+    cipher = ''
+    for k in xrange(block_count):
+        # The algorithm -- > C(i) = Ek( B(i) xor C(i - 1) )
+
+        added_previous = sxor(prev_cipher, block(k)) # B(i) xor C(i - 1)
+        prev_cipher = ecb_encrypt(added_previous, key) # E(--previous line--)
+        
+        cipher += prev_cipher
+
+    return cipher
+
+def cbc_decrypt(iv, cipher, key):
+    
+
+with open('10.txt') as f:
+    cipher = f.read()
